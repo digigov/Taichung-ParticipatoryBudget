7e00591ba742ea30951cffb30d999c33229e2436
TonyQ <tonylovejava@gmal.com>
2016-12-02 14:35:13 +0800

    finetune

--- application/views/pages/qa.php
+++ application/views/pages/qa.php
@@ -2,9 +2,7 @@
 
 <style>
   .question{
-    color:rgb(229,62,94);
     font-size:24px;
-    margin-top:20px;
   }
   .answer{
     font-size:16px;
@@ -21,71 +19,57 @@
       <span class="now"> 問與答 (Q&amp;A)</span>
     </div>
 
-    <div class="col-md-12" style="border:1px solid rgb(110,134,172);padding:5%;border-radius:20px;background:white;">
-      
-      <?php if(false){ ?>
-      <p class="question">Ｑ：為什麼臺中市要推動參與式預算？ </p>
-      <div class="answer">
-        <p><span class="answer-highlight">Ａ：透過參與式預算公共審議的機制達到三大目標 </span></p>
-        <ul>
-          <li>
-          政府公開化： 參與式預算透過公共審議的機制將決策權力交給人民，促使政府提高透明度，將一部分預算的使用模式攤開於全體人民之前，落實開放政府。
-          </li>
-          <li>施政準確化： 參與式預算拉近人民與政府的距離，透過提案反映老百姓的需要，使政府政策能夠符合地方需求，及時改善人民最迫切的需求，施政快又準。</li>
-          <li>民主普遍化： 參與式預算提供一個發聲管道給所有人民，讓他們的需求能被聽見，實踐「城市權」與直接民主的理想。 </li>
+    <div class="col-md-12 qa-container" style="">
 
-        </ul>
-      </div>
-      <p class="question">Ｑ：臺中市參與式預算辦理精神為何？</p>
-      <div class="answer">
-        <p><span class="answer-highlight">Ａ：參與式預算辦理秉持五大原則</span></p>
-        <ul>
-          <li>公民培力：市民才是決定預算分配的主體。</li>
-          <li>公開透明：資訊開放，各項資訊公開大眾檢視。</li>
-          <li>包容多元：尋找傳統政治下被忽略的聲音，提供發聲的管道。</li>
-          <li>立足點式平等：確保每個人都能夠有相等的權力影響公共支出。</li>
-          <li>社區導向：利己利人，從關心社區開始。 </li>
-        </ul>
-      </div>
-      
-      <p class="question">Q：住民會議怎麼開？</p>
-      <div class="answer">
-        <p><span class="answer-highlight">A：</span>住民會議開放給16 歲（含）以上設籍臺中市的市民參與，採取審議民主的精神，大家帶著自己的提案，坐下來和大家分享、彼此交流、相互聆聽，提升提案的公益性，讓議題更能夠貼合大家的需求。 </p>
-      </div>
-      
-      <p class="question">Q：專家學者顧問團的意見我一定要照單全收嗎？ </p>
-      <div class="answer">
-        <p><span class="answer-highlight">A：</span>顧問團的意見回饋主要是協助民眾提高提案的可行性，幫助民眾的提案更完整，也讓政府更容易實踐提案。所有意見都會上網 
-　　公告，要不要採行專家學者顧問團的意見仍由提案人做最終決定。 </p>
-      </div>
-
-      <p class="question">Q：為什麼投票要分兩階段進行？ </p>
-      <div class="answer">
-        <p><span class="answer-highlight">A：</span>今年的採行兩階段投票主要是基於「城市權」的理念，臺中市的發展應該是由全體市民共同決定，因此第一階段投票將由全市 
-　　市民一起決定12 項入圍提案。第二階段的投票將由中區、大里區、豐原區與清水區的居民自行從12 項提案中決選出6項優先 
-　　提案，並且由市府執行之。  </p>
-      </div>
-
-      <p class="question">Q：不會網路投票怎麼辦？  </p>
-      <div class="answer">
-        <p><span class="answer-highlight">A：</span>您可以在投票時間內攜帶身分證明文件到指定地點，當天現場會有志工協助您進行投票喔！（詳細地點都會上網公告，或與 「臺中市參與式預算推動專案辦公室」聯絡。電話04-22221712。）  </p>
-      </div>
-
-      <?php } ?>
-      
-      <?php foreach($qas as $qa){ ?>
-        <p class="question">Q：<?=h($qa->question)?>  </p>
-        <div class="answer">
-          <?=($qa->answer)?>
+      <div class="qa-header clearfix">
+        <div class="qa-left-top">
+          <img src="<?=base_url("img/qa_left_top.png")?>" />
         </div>
-      <?php } ?>
+        <div class="qa-center-top">
+        </div>
+        <div class="qa-right-top">
+          <img src="<?=base_url("img/qa_right_top.png")?>" />
+        </div>
+      </div>
+      <div class="qa-header2 clearfix">
+        <div class="qa-left-middle">
 
-      <div>
-        <hr />
-        <p>
-          有任何參與式預算的相關問題，都歡迎 email 至臺中市參與式預算推動專案辦公室！<br />
-          <a href="mailto:empower.tccentral@gmail.com">empower.tccentral@gmail.com</a>
-        </p>
+        </div>
+        
+        <div class="qa-center-middle" style='width:94%;'>
+          <p><img style='max-width:100%;' src="<?=base_url("img/qa_icon.png")?>" /></p>
+
+          <?php foreach($qas as $qa){ ?>
+          <div class="question-container">
+            <p class="question">Q：<?=h($qa->question)?>  </p>
+            <div class="answer">
+              <?=($qa->answer)?>
+            </div>
+          </div>
+          <?php } ?>
+
+          <div class="answer">
+            <hr />
+            <p>
+              有任何參與式預算的相關問題，都歡迎 email 至臺中市參與式預算推動專案辦公室！<br />
+              <a style='color:white;' href="mailto:empower.tccentral@gmail.com">empower.tccentral@gmail.com</a>
+            </p>
+          </div>  
+        </div>
+        <div class="qa-right-middle">
+          <img src="<?=base_url("img/qa_right_middle.png")?>" />
+        </div>
+        
+      </div>
+      <div class="qa-header clearfix" style='position:relative;'>
+        <div class="qa-left-bottom">
+          <img src="<?=base_url("img/qa_left_bottom.png")?>" />
+        </div>
+        <div class="qa-center-bottom">
+        </div>
+        <div class="qa-right-bottom">
+          <img src="<?=base_url("img/qa_right_bottom.png")?>" />
+        </div>
       </div>
     </div>
     <div style="clear:both"></div>
@@ -95,6 +79,18 @@
 
 <?php function js_section(){ ?>
 
+<script>
+$(".qa-left-middle").css("height",($(".qa-center-middle").height()+5+"px"));
+$(".qa-right-middle").css("height",($(".qa-center-middle").height()+3+"px"));
+
+$(".qa-center-middle").css("width",
+  ($(".qa-header2").width() - 39) +
+  "px");
+
+$(".qa-center-top").css("width",
+  ($(".qa-header2").width() - 85) +
+  "px");
+</script>
 <?php } ?>
 
 
